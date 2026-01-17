69596eb3fc6ba099e4eb664a55a5a708ee223b29
Irina Kievskaya <kievskayaira23@gmail.com>
2026-01-17 18:54:55 +0700

    feat(1:30:25): Добавление модального окна
    MyModal, visible, modal, e.stopPropagation, rootClasses = [cl.myModal]

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
+import MyModal from './components/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,7 +14,8 @@ function App() {
   ]);
 
   const [filter, setFilter] = useState({sort: '', query: ''})
-  
+  const [modal, setModal] = useState(false);
+
   const sortedPosts = useMemo(() => {
     console.log('отработала функция гетсортедпостс')
     if (filter.sort) {
@@ -27,6 +30,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
@@ -35,9 +39,20 @@ function App() {
 
   return (  
     <div className='App'>
-      <PostForm 
-        create={createPost}
-      />
+      <MyButton
+        style={{marginTop: 30}}
+        onClick={() => setModal(true)}
+      >
+        Создать пост
+      </MyButton>
+      <MyModal
+        visible={modal}
+        setVisible={setModal}
+      >
+        <PostForm 
+          create={createPost}
+        />
+      </MyModal>
       <hr style={{margin: '15px 0'}}></hr>
       <PostFilter 
         filter={filter}
